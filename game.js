89c6658549603132c7419f2b7c5add85cc703c98
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-01-12 17:11:12 -0300

    Se acomoda tamaños

--- game.js
+++ game.js
@@ -1,10 +1,32 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', starGame);
+window.addEventListener('load', startGame);
 
-function starGame() {
-    game.fiilRect(0,0,100,100) //definimos el lugar donde iniciar el trazo (cualquier cosa)
+function startGame() {
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    }else{
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementSize = canvasSize / 10;
+
+    console.log({canvasSize, elementSize});
+
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++){
+        game.fillText(emojis['X'], elementSize * 1, elementSize);
+    }
+}
+/*     game.fiilRect(0,0,100,100) //definimos el lugar donde iniciar el trazo (cualquier cosa)
     game.clearRect(50,50,50,50)  // borramos trazo
     game.clearRect()
     game.clearRect(0,0,50,50) 
@@ -12,5 +34,4 @@ function starGame() {
     game.font = '25px Verdana'
     game.fillStyle = 'purple';
     game.textAlign = 'left'
-    game.fillText('Platzi', 25, 25)
-}
+    game.fillText('Platzi', 25, 25) */
