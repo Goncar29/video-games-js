1d457a64e06ce96f64f1c294a0f55219a9d047a4
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-01-16 14:01:07 -0300

    Refactor del mapa de juego

--- game.js
+++ game.js
@@ -33,11 +33,21 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split('')); // ahora quitamos los espacios a cada array y separamos por cada elemento
     console.log({map, mapRows, mapRowCols});
 
-    for (let row = 1; row <= 10; row++){
-        for (let col = 1; col <= 10; col++){
-            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-        }
-    }
+    mapRowCols.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) =>{
+            const emoji = emojis[col];
+            const posX = elementsSize * (colIndex + 1);
+            const posY = elementsSize * (rowIndex + 1);
+            game.fillText(emoji, posX, posY);
+            console.log({ row, rowIndex, col, colIndex });
+        });
+    });
+
+    // for (let row = 1; row <= 10; row++){
+    //     for (let col = 1; col <= 10; col++){
+    //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+    //     }
+    // }
 }
 
 
