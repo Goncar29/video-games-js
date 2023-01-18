3b94a80e77b20935ea28ed2a1947a2566f933915
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-01-18 14:40:24 -0300

    Objeto playerPosition, posicionamos al jugador, lo renderizamos y movimiento del mismo

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+};
 
 window.addEventListener('load', startGame);
 window.addEventListener('resize', setCanvasSize);
@@ -28,33 +37,50 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map = maps[2];
+    const map = maps[0]; // Elegimos el mapa
     const mapRows = map.trim().split('\n'); // quitamos los espacios vacios al inicio y final y los convertimos en array
     const mapRowCols = mapRows.map(row => row.trim().split('')); // ahora quitamos los espacios a cada array y separamos por cada elemento
     console.log({map, mapRows, mapRowCols});
-
+    
+    // Posicionamos cada elemento del mapa 
     mapRowCols.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) =>{
             const emoji = emojis[col];
             const posX = elementsSize * (colIndex + 1);
             const posY = elementsSize * (rowIndex + 1);
+        // Esto es lo mismo que el forEach de arriba
+        // for (let row = 1; row <= 10; row++){
+        //     for (let col = 1; col <= 10; col++){
+        //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+        //     }
+        // }
+
+            //  Aqui debe de ir el jugador
+            if(col == 'O'){
+                console.log({ posX, posY });
+                playerPosition.x = posX;
+                playerPosition.y = posY;
+                console.log({playerPosition});
+            }
+
+            //  Renderizamos el mapa con los emojis correspondiente del mapa
             game.fillText(emoji, posX, posY);
             console.log({ row, rowIndex, col, colIndex });
         });
     });
 
-    // for (let row = 1; row <= 10; row++){
-    //     for (let col = 1; col <= 10; col++){
-    //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-    //     }
-    // }
+    movePlayer();
+}
+//  Renderizamos al jugador
+function movePlayer(){
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
 window.addEventListener('keydown', moveByKeys);
 btnUp.addEventListener('click', moveUp);
 btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
-btnUDown.addEventListener('click', moveDown);
+btnDown.addEventListener('click', moveDown);
 
 function moveByKeys(event){
     if (event.key == 'ArrowUp') moveUp();
@@ -64,6 +90,8 @@ function moveByKeys(event){
 }
 function moveUp(){
     console.log('Me quiero mover hacia arriba')
+    playerPosition.y -= elementsSize;
+    movePlayer();
 }
 function moveLeft(){
     console.log('Me quiero mover hacia la izquierda')
